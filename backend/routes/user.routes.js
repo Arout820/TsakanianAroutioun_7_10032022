ade936f56dbb1a5c8215580b21757b1c2257add5
Aroutioun Tsakanian <aroutioun.tsakanian@gmail.com>
2022-03-12 17:00:20 +0100

    add of password-validator plugin to chech password

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/user.controllers');
+const password = require('../middleware/password.validation');
 
-router.post('/signup', userController.signup);
+router.post('/signup', password, userController.signup);
 router.post('/login', userController.login);
 
 module.exports = router;
