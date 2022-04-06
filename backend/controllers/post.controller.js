9dd07d9bcb72c20947dad7a5c0b6c7c2df2d5d28
Aroutioun Tsakanian <aroutioun.tsakanian@gmail.com>
2022-04-06 07:43:55 +0200

    BACKEND: modification of sql request for post controller

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -2,17 +2,18 @@ const database = require('../config/db');
 
 // Class post pour données à envoyer à la BDD
 class Post {
-  constructor(content, user_id) {
+  constructor(content, user_id, attachment) {
     this.content = content;
     this.user_id = user_id;
+    this.attachment = attachment;
   }
 }
 
 // ----------------------- Créer un post ----------------------- //
 exports.createPost = (req, res) => {
-  const { content, user_id } = req.body;
-  const post = new Post(content, attachment, user_id);
-  database.query('INSERT INTO post VALUES(\'\',\''+content+'\',\''+attachment+'\',\'\',\''+user_id+'\' )', post, (error, results) => {
+  const { content, user_id, attachment } = req.body;
+  const post = new Post(content, user_id, attachment);
+  database.query('INSERT INTO post SET ?', post, (error, results) => {
     if (error) {
       console.log(error);
       return res.status(400).json({ error });
@@ -23,24 +24,31 @@ exports.createPost = (req, res) => {
 
 // ------------------ Récuperer tous les posts ------------------ //
 exports.getAllPost = (req, res) => {
-  database.query('SELECT * FROM post', (error, results) => {
-    if (error) {
-      console.log(error);
-      return res.status(400).json({ error });
+  database.query(
+    'SELECT post_id, content, attachment, post_create_time, post.user_id, firstname, lastname, isAdmin FROM post JOIN user ON post.user_id = user.user_id',
+    (error, results) => {
+      if (error) {
+        console.log(error);
+        return res.status(400).json({ error });
+      }
+      res.status(200).json(results);
     }
-    res.status(200).json(results);
-  });
+  );
 };
 
 // ---------------------- Récuperer un post ---------------------- //
 exports.getOnePost = (req, res) => {
-  database.query('SELECT * FROM post WHERE post_id = ?', req.params.id, (error, results) => {
-    if (error) {
-      console.log(error);
-      return res.status(400).json({ error });
+  database.query(
+    'SELECT post_id, content, attachment, post_create_time, post.user_id, firstname, lastname, isAdmin FROM post JOIN user ON post.user_id = user.user_id WHERE post_id = ?',
+    req.params.id,
+    (error, results) => {
+      if (error) {
+        console.log(error);
+        return res.status(400).json({ error });
+      }
+      res.status(200).json(results);
     }
-    res.status(200).json(results);
-  });
+  );
 };
 
 // ---------------------- Modifier un post ---------------------- //
