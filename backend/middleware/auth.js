be2a908673c82e88268048255bf33657d7401b45
Aroutioun Tsakanian <aroutioun.tsakanian@gmail.com>
2022-04-12 04:59:15 +0200

    correction of an authentification mistake

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -7,12 +7,12 @@ module.exports = (req, res, next) => {
     const token = req.headers.authorization.split(' ')[1];
     const decodedToken = jwt.verify(token, process.env.TOKEN);
     const userId = decodedToken.userId;
-    if (req.body.userId && req.body.userId !== userId) {
-      throw 'UserId non valable !';
+    if (req.body.user_id && req.body.user_id != userId) {
+      throw 'User_id non valable !';
     } else {
       next();
     }
   } catch (error) {
-    res.status(403).json({ error: 'Requête non authentifiée !' });
+    res.status(403).json({ error: error || 'Requête non authentifiée' });
   }
 };
